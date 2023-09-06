26797bd584e991aad71233e7a1ba0de82cbb4759
pappudey <pappudey.coder@gmail.com>
2023-09-06 11:13:50 +0600

    Menu Updated

--- client/src/components/menu/Menu.jsx
+++ client/src/components/menu/Menu.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineMenu } from "react-icons/ai";
 import { FaBasketShopping, FaPhoneVolume } from "react-icons/fa6";
 import LogoImg from "../../assets/logo.png";
+
+
+
+    
 const Menu = () => {
+    let [scrollPosi , setScrollPosi ] = useState(0);
+    let [isSticky , setIsSticky ] = useState(false);
+
+    window.addEventListener('scroll' , () =>{
+        let scrollPosition = window.scrollY;
+        setIsSticky(scrollPosition  > 0)
+    }) 
+
   return (
     // menu section 
-    <section className="z-50 fixed w-full" >
+    <section className={isSticky ?  'menu sticky top-0 z-50 w-full lg:h-[100px] bg-zinc-50   shadow-[2px 4px 6px 0px rgba(12, 0, 46, 0.05)] ease-in duration-500 ' : 'ease-in duration-300' } >
         
+        <div className='z-50 top-0 absolute w-full ease-in duration-300 lg:h-[100px]'>
         <div className='  lg:w-[1300px]  w-4/5 lg:h-[100px] h-[80px] m-auto flex flex-wrap items-center px-[7px]'>
             {/* navbar logo section  */}
             <div className='lg:w-[16%] w-[55%]'>
@@ -45,6 +58,7 @@ const Menu = () => {
             <div className='w-[5%] lg:hidden pl-3 text-center h-auto'>
                 <AiOutlineMenu className='text-2xl'/>
             </div>
+        </div>
         </div>
 
     </section>
