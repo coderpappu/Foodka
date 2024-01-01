e7559e7b8380db07a2b5e9ce02c64de6c92b6c95
pappudey <pappudey.coder@gmail.com>
2024-01-01 19:25:35 +0600

    background image problem solved. we solve it by using cloudinary

---
+++ client/src/components/excellentFood/ExcellentFood.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ExBgimg from "../../assets/exbg.png";
+
+const ExcellentFood = () => {
+  return (
+    <div
+      className={`w-full h-[500px] bg-[url('https://res.cloudinary.com/dpfpz6upr/image/upload/v1704115474/exbg_f6gujs.png')]`}
+    >
+      <div className="responsive-wrapper mt-20"></div>
+    </div>
+  );
+};
+
+export default ExcellentFood;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,7 @@ import Intro from "../components/intro/Intro";
 import Menu from "../components/menu/Menu";
 import OfferArea from "../components/offer-area/OfferArea";
 import SignatureFood from "../components/food/SignatureFood";
+import ExcellentFood from "../components/excellentFood/ExcellentFood.jsx";
 const Home = () => {
   return (
     <>
@@ -15,6 +16,7 @@ const Home = () => {
       <About />
       <OfferArea />
       <SignatureFood />
+      <ExcellentFood />
 
       <div className="h-[200px]"></div>
     </>
